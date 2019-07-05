d6782365d2d7cb4dd77d313bd753dac595e169b4
Shimakaze <MyEmail>
2019-07-05 20:00:19 +0200

    Add navigation component into app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from "react";
 import "./App.css";
+import Navigation from "../components/Navigation";
 
 class App extends React.Component {
   state = {};
   render() {
-    return <></>;
+    return (
+      <>
+        <Navigation />
+      </>
+    );
   }
 }
 
