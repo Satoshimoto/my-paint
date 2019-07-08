1cd560f5e5803efeae2f2c803189fd104dd0ff74
Shimakaze <MyEmail>
2019-07-08 15:57:28 +0200

    Add container and switcher to dark mode

--- src/components/Canvas.js
+++ src/components/Canvas.js
@@ -1,8 +1,17 @@
-import React from 'react';
+import React from "react";
 import "../scss/Canvas.scss";
 
 const Canvas = () => {
-    return (  );
-}
- 
+  return (
+    <>
+      <div className="container">
+        <h3 className="container__header">Dark mode</h3>
+        <div className="switch" onClick={handleSwitchMode}>
+          <span className="switch--mode" />
+        </div>
+      </div>
+    </>
+  );
+};
+
 export default Canvas;
