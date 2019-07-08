4a7278ee82f18a2f464c014731d55dcbacbf50bc
Shimakaze <MyEmail>
2019-07-08 23:11:28 +0200

    Add container to create canvas

--- src/components/Canvas.js
+++ src/components/Canvas.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "../scss/Canvas.scss";
 
-const Canvas = () => {
+const Canvas = props => {
   function handleSwitchMode() {
     let container = document.querySelector(".container");
     let header = document.querySelector(".container__header");
@@ -28,6 +28,45 @@ const Canvas = () => {
         <div className="switch" onClick={handleSwitchMode}>
           <span className="switch--mode" />
         </div>
+
+        {props.step === 0 && !props.created ? (
+          <div className="container__create">
+            <div className="add" onClick={e => props.create(e)}>
+              <i className="fas fa-plus" />
+              <h4>Create new canvas</h4>
+            </div>
+          </div>
+        ) : props.step === 1 && !props.created ? (
+          <div className="container__create">
+            <div className="container__properties">
+              <p>Canvas Width:</p>
+              <input
+                type="number"
+                min="50"
+                max="2000"
+                className="container__properties--input width"
+                onChange={e => props.setDimensions(e)}
+              />
+            </div>
+            <div className="container__properties">
+              <p className="container__properties"> Canvas Height:</p>
+              <input
+                type="number"
+                min="50"
+                max="2000"
+                className="container__properties--input height"
+                onChange={e => props.setDimensions(e)}
+              />
+            </div>
+
+            <div
+              className="container__properties--button"
+              onClick={e => props.create(e)}
+            >
+              <p>Create</p>
+            </div>
+          </div>
+        ) : null}
       </div>
     </>
   );
