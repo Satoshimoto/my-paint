be225d83db56044821836635ae65fb34209a7dff
Shimakaze <MyEmail>
2019-07-08 02:16:30 +0200

    Add handle open navigation

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -2,6 +2,30 @@ import React from "react";
 import "../scss/Navigation.scss";
 
 const Navigation = props => {
+  function handleNavigation() {
+    let header = document.querySelector(".header");
+    let grasper = document.querySelector(".header .header__grasper");
+    let grasperArrow = document.querySelector(
+      ".header__grasper .header__grasper--arrow"
+    );
+    let tools = document.querySelector(".navigation .navigation__tools");
+    let properties = document.querySelector(".navigation__properties");
+
+    if (header.offsetWidth === 15) {
+      header.style.width = "200px";
+      grasper.style.left = "185px";
+      tools.style.display = "flex";
+      properties.style.display = "block";
+      grasperArrow.style.animation = "grasperOpen 0.4s forwards ease-in-out";
+    } else {
+      header.style.width = "15px";
+      grasper.style.left = "0px";
+      tools.style.display = "none";
+      properties.style.display = "none";
+      grasperArrow.style.animation = "grasperClose 0.4s forwards ease-in-out";
+    }
+  }
+
   return (
     <>
       <header className="header">
