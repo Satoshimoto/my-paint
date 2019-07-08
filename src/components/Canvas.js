e3183d80656c0253f35fb927ee81802f43d713e1
Shimakaze <MyEmail>
2019-07-08 15:59:16 +0200

    Add handle switch mode function

--- src/components/Canvas.js
+++ src/components/Canvas.js
@@ -2,6 +2,25 @@ import React from "react";
 import "../scss/Canvas.scss";
 
 const Canvas = () => {
+  function handleSwitchMode() {
+    let container = document.querySelector(".container");
+    let header = document.querySelector(".container__header");
+    let switchContainer = document.querySelector(".switch");
+    let switchMode = document.querySelector(".switch .switch--mode");
+
+    if (switchMode.offsetLeft === 4) {
+      switchMode.style.left = "30px";
+      switchContainer.style.backgroundColor = "#187ac6";
+      container.style.backgroundColor = "#333";
+      header.style.color = "#fff";
+    } else {
+      switchMode.style.left = "4px";
+      switchContainer.style.backgroundColor = "#ccc";
+      container.style.backgroundColor = "#fff";
+      header.style.color = "#000";
+    }
+  }
+
   return (
     <>
       <div className="container">
