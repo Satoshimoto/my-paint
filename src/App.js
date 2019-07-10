cced6def3ffc5476a38300917e4744da7914b4f3
Shimakaze <MyEmail>
2019-07-10 15:29:52 +0200

    Change default value of canvas width and height and delete function to set demension of canvas

--- src/App.js
+++ src/App.js
@@ -62,8 +62,6 @@ class App extends React.Component {
   };
 
   handleCreateCanvas = e => {
-    let container = document.querySelector(".container");
-
     if (e.currentTarget.classList[0] === "add") {
       this.setState({
         createStep: 1
@@ -74,11 +72,19 @@ class App extends React.Component {
       let height = document.querySelector(".height").value;
 
       if (height === "") {
-        height = 50;
+        height = 10;
+      } else if (height < 10) {
+        height = 10;
+      } else if (height > 2000) {
+        height = 2000;
       }
 
       if (width === "") {
-        width = 50;
+        width = 10;
+      } else if (width < 10) {
+        width = 10;
+      } else if (width > 2000) {
+        width = 2000;
       }
 
       this.setState({
@@ -86,28 +92,6 @@ class App extends React.Component {
         created: true,
         canvasWidth: width + "px",
         canvasHeight: height + "px"
-      });
-    }
-  };
-
-  handleCanvasDimensions = e => {
-    if (e.currentTarget.classList[1] === "width") {
-      if (e.currentTarget.value < 50) {
-        e.currentTarget.value = 50;
-      } else if (e.currentTarget.value > 2000) {
-        e.currentTarget.value = 2000;
-      }
-      this.setState({
-        canvasWidth: e.currentTarget.value
-      });
-    } else {
-      if (e.currentTarget.value < 50) {
-        e.currentTarget.value = 50;
-      } else if (e.currentTarget.value > 2000) {
-        e.currentTarget.value = 2000;
-      }
-      this.setState({
-        canvasHeight: e.currentTarget.value
       });
     }
   };
