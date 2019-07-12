55a5f9494880c574a1d157eefd28ff29a05a4055
Shimakaze <MyEmail>
2019-07-12 21:11:07 +0200

    Updates

---
+++ precache-manifest.7265a64eab9da4389bccdee206f0ebd6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aae6eb4c108509de16a2da587e995ea4",
+    "url": "/my-paint/index.html"
+  },
+  {
+    "revision": "6f0a4927aedd25354336",
+    "url": "/my-paint/static/css/main.49b8261b.chunk.css"
+  },
+  {
+    "revision": "c9a18af572e99c757ad5",
+    "url": "/my-paint/static/js/2.17573368.chunk.js"
+  },
+  {
+    "revision": "6f0a4927aedd25354336",
+    "url": "/my-paint/static/js/main.eada3406.chunk.js"
+  },
+  {
+    "revision": "9a76570ea15474db6f22",
+    "url": "/my-paint/static/js/runtime~main.32a668f2.js"
+  }
+]);
